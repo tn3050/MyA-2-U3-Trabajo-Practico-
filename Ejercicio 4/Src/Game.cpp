5294a878b3fe877dc204598f5013c41e7bcc3321
tn3050 <vtom3092@gmail.com>
2025-04-12 21:00:17 -0300

    Add files via upload

---
+++ Ejercicio 4/Src/Game.cpp
@@ -0,0 +1,133 @@
+﻿#include "Game.h"
+#include "Box2DHelper.h"
+
+// Constructor de la clase Game
+Game::Game(int ancho, int alto, std::string titulo)
+{
+	wnd = new RenderWindow(VideoMode(ancho, alto), titulo); // Crea una ventana con las dimensiones y título especificados
+	wnd->setVisible(true); // Hace visible la ventana
+	fps = 60; // Establece el límite de cuadros por segundo (FPS)
+	wnd->setFramerateLimit(fps); // Aplica el límite de FPS a la ventana
+	frameTime = 1.0f / fps; // Calcula el tiempo por cuadro en segundos
+	SetZoom(); // Configura el "zoom" o vista de la cámara
+	InitPhysics(); // Inicializa la simulación de física
+}
+
+void Game::Loop()
+{
+	while (wnd->isOpen()) // Bucle principal del juego que se ejecuta mientras la ventana esté abierta
+	{
+		wnd->clear(clearColor); // Limpia la ventana con el color de fondo
+		DoEvents(); // Maneja los eventos (input del usuario)
+		CheckCollitions(); // Verifica colisiones (a implementar)
+		UpdatePhysics(); // Actualiza la simulación de física
+		DrawGame(); // Dibuja los elementos del juego
+		wnd->display(); // Muestra los cambios en la ventana
+	}
+}
+
+void Game::UpdatePhysics()
+{
+	phyWorld->Step(frameTime, 8, 8); // Avanza la simulación de física un paso
+	phyWorld->ClearForces(); // Limpia las fuerzas acumuladas
+	phyWorld->DebugDraw(); // Dibuja la representación de debug de la simulación
+}
+
+
+void Game::DrawGame()
+{
+	// Función para dibujar los elementos del juego (a implementar)
+}
+
+void Game::DoEvents()
+{
+	Event evt;
+	while (wnd->pollEvent(evt)) // Procesa todos los eventos acumulados
+	{
+		switch (evt.type)
+		{
+		case Event::Closed: // Si se solicita cerrar la ventana
+			wnd->close(); // Cierra la ventana
+			break;
+		case Event::MouseButtonPressed: // Si se presiona un botón del ratón
+			// Crea un cuerpo dinámico con forma triangular en la posición del clic
+			b2Body* body = Box2DHelper::CreateTriangularDynamicBody(phyWorld, b2Vec2(0.0f, 0.0f), 10.0f, 1.0f, 4.0f, 0.1f);
+			Vector2f pos = wnd->mapPixelToCoords(Vector2i(evt.mouseButton.x, evt.mouseButton.y)); // Transforma la posición del clic a coordenadas del mundo
+			body->SetTransform(b2Vec2(pos.x, pos.y), 0.0f); // Mueve el cuerpo a la posición del clic
+			break;
+		}
+	}
+	const float fuerza = 250.0f;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		cuadrado->ApplyForceToCenter(b2Vec2(-fuerza, 0.0f), true);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		cuadrado->ApplyForceToCenter(b2Vec2(fuerza, 0.0f), true);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		cuadrado->ApplyForceToCenter(b2Vec2(0.0f, -fuerza), true);
+
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		cuadrado->ApplyForceToCenter(b2Vec2(0.0f, fuerza), true);
+	}
+
+}
+
+void Game::CheckCollitions()
+{
+	// Verificación de colisiones (a implementar)
+}
+
+void Game::SetZoom()
+{
+	View camara;
+	camara.setSize(100.0f, 100.0f); // Establece el tamaño de la vista
+	camara.setCenter(50.0f, 50.0f); // Centra la vista en un punto del mundo
+	wnd->setView(camara); // Aplica la vista a la ventana
+}
+
+void Game::InitPhysics()
+{
+	// Inicializa el mundo de Box2D con una gravedad hacia abajo
+	phyWorld = new b2World(b2Vec2(0.0f, 9.8f));
+
+	debugRender = new SFMLRenderer(wnd); // Crea un renderizador de debug para SFML
+	debugRender->SetFlags(UINT_MAX); // Configura el renderizador para dibujar todas las formas de debug
+	phyWorld->SetDebugDraw(debugRender); // Establece el renderizador de debug para el mundo de Box2D
+
+	// Crea cuerpos estáticos para simular el suelo y las paredes
+	// También crea cuerpos dinámicos (como círculos) y los une con resortes al techo para demostrar la simulación física
+	b2Body* groundBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 100, 10);
+	groundBody->SetTransform(b2Vec2(50.0f, 100.0f), 0.0f);
+
+	b2Body* leftWallBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 10, 100);
+	leftWallBody->SetTransform(b2Vec2(0.0f, 50.0f), 0.0f);
+
+	b2Body* rightWallBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 10, 100);
+	rightWallBody->SetTransform(b2Vec2(100.0f, 50.0f), 0.0f);
+
+	// Creamos un techo
+	b2Body* topWallBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 100, 10);
+	topWallBody->SetTransform(b2Vec2(50.0f, 0.0f), 0.0f);
+
+
+
+
+
+	cuadrado = Box2DHelper::CreateRectangularDynamicBody(phyWorld, 5.0f, 5.0f, 1.0f, 0.1f, 0.5f);
+	cuadrado->SetTransform(b2Vec2(50.0f, 80.0f), 0.0f);
+
+	
+	// Cabeza Atamos la esfera mediante un resorte al techo. Este resorte se encuentra dampeado totalmente
+
+
+
+
+}
+
+Game::~Game(void)
+{
+	// Destructor de la clase Game (a implementar si es necesario)
+}
---
+++ Ejercicio 4/Src/Game.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include "SFMLRenderer.h"
+#include <list>
+
+using namespace sf;
+class Game
+{
+private:
+	// Propiedades de la ventana
+	int alto;
+	int ancho;
+	RenderWindow* wnd;
+	Color clearColor;
+
+	// Objetos de box2d
+	b2World* phyWorld;
+	b2Body* cuadrado;
+	SFMLRenderer* debugRender;
+
+	// Tiempo de frame
+	float frameTime;
+	int fps;
+
+public:
+	//Constructores, destructores e inicializadores
+	Game(int ancho, int alto, std::string titulo);
+	void CheckCollitions();
+	void CreateEnemy(int x, int y);
+	~Game(void);
+	void InitPhysics();
+
+	// Main game loop
+	void Loop();
+	void DrawGame();
+	void UpdatePhysics();
+	void DoEvents();
+	void SetZoom();
+};
